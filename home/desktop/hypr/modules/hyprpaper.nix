4bd2c0ddc5465c80e78a0045a56a6d10b6ef9d4e
venefex <git@alexdidom.com>
2026-08-03 23:31:45 +0200

    added hyprpaper

--- home/desktop/hypr/modules/hyprpaper.nix
+++ home/desktop/hypr/modules/hyprpaper.nix
@@ -11,7 +11,7 @@
       wallpaper = [
         {
           monitor = "DP-1";
-          path = ../../wallpapers/mountains.jpg;
+          path = "${../../wallpapers/mountains.jpg}";
           fit_mode = "cover";
         }
       ];
