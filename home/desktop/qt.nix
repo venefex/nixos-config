cef7aefdfdf3c936db5aaeb8c0e94d946e4290a9
venefex <git@alexdidom.com>
2026-08-03 16:26:11 +0200

    added qt5ct settings

--- home/desktop/qt.nix
+++ home/desktop/qt.nix
@@ -1,9 +1,45 @@
-{ pkgs,  ... }:
+{ pkgs, ... }:
 
 {
   qt = {
     enable = true;
     platformTheme.name = "qtct";
+
+    qt5ctSettings = {
+      Appearance = {
+        color_scheme_path = "${pkgs.kdePackages.breeze}/share/color-schemes/BreezeDark.colors";
+        custom_palette = true;
+        icon_theme = "breeze-dark";
+        standard_dialogs = "default";
+        style = "Breeze";
+      };
+
+      Fonts = {
+        fixed = "\"JetBrains Mono,10,-1,5,400,0,0,0,0,0,0,0,0,0,0,1\"";
+        general = "\"Noto Sans,10,-1,5,400,0,0,0,0,0,0,0,0,0,0,1\"";
+      };
+
+      Interface = {
+        activate_item_on_single_click = true;
+        buttonbox_layout = 2;
+        cursor_flash_time = 1000;
+        dialog_buttons_have_icons = true;
+        double_click_interval = 400;
+        gui_effects = "@Invalid()";
+        keyboard_scheme = 2;
+        menus_have_icons = true;
+        show_shortcuts_in_context_menus = true;
+        stylesheets = "@Invalid()";
+        toolbutton_style = 4;
+        underline_shortcut = true;
+        wheel_scroll_lines = 3;
+      };
+
+      Troubleshooting = {
+        force_raster_widgets = true;
+        ignored_applications = "@Invalid()";
+      };
+    };
 
     qt6ctSettings = {
       Appearance = {
