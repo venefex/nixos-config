60b5682b07e7fc36e80522f800d9e8497064c473
venefex <git@alexdidom.com>
2026-08-03 15:22:14 +0200

    updated tree structure

---
+++ home/core/packages.nix
@@ -0,0 +1,41 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    # Applications
+    brave
+    kdePackages.ark
+    kdePackages.dolphin
+    kdePackages.konsole
+    kdePackages.okular
+    kdePackages.kcalc
+    kdePackages.kompare
+    liquidctl
+    vial
+    xnviewmp
+
+    # Dolphin integration
+    kdePackages.dolphin-plugins
+    kdePackages.ffmpegthumbs
+    kdePackages.kio-admin
+    kdePackages.kdegraphics-thumbnailers
+    kdePackages.kservice
+
+    # Qt / Gnome theming
+    kdePackages.breeze
+    kdePackages.breeze-gtk
+    kdePackages.breeze-icons
+    nwg-look
+
+    # Image format support
+    icoutils
+    kdePackages.qtsvg
+    kdePackages.qtimageformats
+    kdePackages.kimageformats
+    resvg
+
+    # Misc
+    runapp
+    pavucontrol
+  ];
+}
