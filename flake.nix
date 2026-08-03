3d919e6e42acaecf3d0092234fb8141b2ef24839
venefex <git@alexdidom.com>
2026-08-03 23:53:00 +0200

    added noctalia to flake

--- flake.nix
+++ flake.nix
@@ -15,6 +15,10 @@
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia";
+      inputs.nixpkgs.follows = "nixpkgs"; # this line is optional, prevents downloading two versions of nixpkgs but disables cache
+    };
   };
 
   outputs =
