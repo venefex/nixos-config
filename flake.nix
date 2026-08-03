da1e503a7dea3cccf780c6264cff990b6504c5a9
venefex <git@alexdidom.com>
2026-08-04 00:12:39 +0200

    added noctalia

--- flake.nix
+++ flake.nix
@@ -17,7 +17,12 @@
     };
     noctalia = {
       url = "github:noctalia-dev/noctalia/cachix";
-      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixConfig = {
+      extra-substituters = [ "https://noctalia.cachix.org" ];
+      extra-trusted-public-keys = [
+        "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+      ];
     };
   };
 
