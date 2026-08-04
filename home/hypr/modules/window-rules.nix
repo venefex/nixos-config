f93feccbbb7e2a217c904712b91c3814613e1671
venefex <git@alexdidom.com>
2026-08-04 23:48:17 +0200

    tweaking passwordManager windowrules

--- home/hypr/modules/window-rules.nix
+++ home/hypr/modules/window-rules.nix
@@ -5,8 +5,8 @@
       match.class = "org.keepassxc.KeePassXC";
       float = true;
       size = [
-        "monitor_w*0.4"
-        "monitor_h*0.5"
+        "monitor_w*0.6"
+        "monitor_h*0.8"
       ];
       center = true;
     };
