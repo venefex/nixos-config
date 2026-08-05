1dda6e7cc5a94a8c36d8760811645d9e74cbc357
venefex <git@alexdidom.com>
2026-08-05 16:43:17 +0200

    added bash settings

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -4,6 +4,11 @@
   programs.bash = {
     enable = true;
 
+    # History
+    historyControl = [ "ignoreboth" ];
+    historySize = 1000000;
+    historyFileSize = 1000000;
+
     shellAliases = {
       # System
       cat = "bat";
@@ -15,12 +20,26 @@
       nupl = "systemctl status nixos-upgrade.service";
     };
 
+    shellOptions = [
+      "histappend" # Append to the history file, don't overwrite it
+      "checkwinsize" # Better window resizing
+      "cmdhist" # Multi-line command history
+      "globstar" # Recursive globbing
+    ];
+
     bashrcExtra = ''
+      # Editor
       if command -v micro >/dev/null 2>&1; then
           export EDITOR="micro"
       else
           export EDITOR="nano"
       fi
+
+      # Set to 0 for plain-text separators without Powerline symbols
+      export USE_POWERLINE=1
+
+      # Prompt configuration
+      PROMPT_DIRTRIM=2
     '';
 
     initExtra = ''
