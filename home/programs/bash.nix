8e93df66acad25810beb968a705317c0adb2dfdb
venefex <git@alexdidom.com>
2026-08-05 15:38:07 +0200

    added a git alias

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -3,12 +3,27 @@
 {
   programs.bash = {
     enable = true;
+
     shellAliases = {
+      # System
       cat = "bat";
+
+      # NixOS
       nrs = "sudo nixos-rebuild switch --flake ~/.nixos-config";
       nrsv = "sudo nixos-rebuild switch --flake ~/.nixos-config --verbose";
       nrsr = "sudo nixos-rebuild switch --flake ~/.nixos-config --rollback";
       nupl = "systemctl status nixos-upgrade.service";
+
+      # Git
+      gac = "__gac";
     };
+
+    initExtra = ''
+      __gac() {
+        git add . || return
+        read -rp "Commit message: " msg
+        git commit -m "$msg"
+      }
+    '';
   };
 }
