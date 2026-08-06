f9791853d92c97e923a3f3c8696da009dabf5e48
venefex <git@alexdidom.com>
2026-08-07 00:56:17 +0200

    imported auto-start.nix

--- home/hyprland/hyprland.nix
+++ home/hyprland/hyprland.nix
@@ -4,6 +4,7 @@
   # Installation is in ./modules/desktop/hyprland.nix
 
   imports = [
+    ./modules/autostart.nix
     ./modules/keybinds.nix
     ./modules/monitors.nix
     ./modules/hyprpaper.nix
