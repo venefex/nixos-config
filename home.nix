eac03e47477d8dd474bf2f695dc1eea71dd82730
venefex <git@alexdidom.com>
2026-08-06 10:34:31 +0200

    home-manager config tidy-up

--- home.nix
+++ home.nix
@@ -1,60 +1,40 @@
-{ config, pkgs, ... }:
+{ lib, ... }:
 
+let
+  programsDir = ./home/programs;
+  programModules = lib.mapAttrsToList (name: _: programsDir + "/${name}") (
+    lib.filterAttrs (name: type: type == "regular" && lib.hasSuffix ".nix" name) (
+      builtins.readDir programsDir
+    )
+  );
+in
 {
   imports = [
+    # Core hm settings
+    ./home/core/core.nix
+
     # Backup files cleaner
-    ./home/core/hm-backup-cleanup.nix
+    ./home/services/backup-cleanup.nix
 
     # Desktop
     ./home/desktop/qt.nix
     ./home/desktop/cursor.nix
     ./home/desktop/xdg.nix
 
-    # Programs
-    ./home/programs/bash.nix
-    ./home/programs/bat.nix
-    ./home/programs/btop.nix
-    # ./home/programs/cmus.nix
-    ./home/programs/firefox.nix
-    ./home/programs/git.nix
-    ./home/programs/keepassxc.nix
-    ./home/programs/kitty.nix
-    ./home/programs/lesspipe.nix
-    # ./modules/home/programs/librewolf.nix
-    ./home/programs/micro.nix
-    ./home/programs/mpv.nix
-    ./home/programs/qalculate.nix
-    ./home/programs/tealdeer.nix
-    ./home/programs/vscode.nix
-    ./home/programs/yt-dlp.nix
-
+    # Programs (every .nix file under home/programs/)
+  ]
+  ++ programModules
+  ++ [
     ./home/core/packages.nix
     ./home/core/variables.nix
-
     # Hypr
     ./home/hypr/hyprland.nix
-
     # Noctalia
     ./home/desktop/noctalia.nix
   ];
 
   home.username = "alex";
   home.homeDirectory = "/home/alex";
-
-  services = {
-    hmBackupCleanup = {
-      enable = true;
-      prefix = "hm-backup";
-      retentionDays = 30;
-      calendar = "weekly";
-    };
-  };
-
-  programs = {
-    neovim = {
-      enable = true;
-    };
-  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
