36b7e6d2814ec5995d262a3894d348cab3973bf5
venefex <git@alexdidom.com>
2026-08-06 19:49:40 +0200

    fix home.yaml path

--- home/security/secrets.nix
+++ home/security/secrets.nix
@@ -5,7 +5,7 @@
     age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
 
     secrets.github_ssh_key = {
-      sopsFile = ./secrets/home.yaml;
+      sopsFile = ../../secrets/home.yaml;
       path = "${config.home.homeDirectory}/.ssh/github_ed25519";
       mode = "0600";
     };
