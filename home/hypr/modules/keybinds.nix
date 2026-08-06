bfcdbb261ff48526e294b095622986a32c12f29c
venefex <git@alexdidom.com>
2026-08-06 10:14:30 +0200

    automated hyprland keybinds import

--- home/hypr/modules/keybinds.nix
+++ home/hypr/modules/keybinds.nix
@@ -1,7 +1,12 @@
-{ ... }:
+{ lib, ... }:
 
+let
+  dir = ./keybinds;
+  # every regular .nix file in the directory, excluding this aggregator if it ever lands there
+  files = lib.filterAttrs (name: type: type == "regular" && lib.hasSuffix ".nix" name) (
+    builtins.readDir dir
+  );
+in
 {
-  imports = [
-    ./keybinds/programs.nix
-  ];
+  imports = lib.mapAttrsToList (name: _: dir + "/${name}") files;
 }
