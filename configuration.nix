9963622b6d6c020efa73dfc874b84ba4d52c1a6c
venefex <git@alexdidom.com>
2026-08-06 18:12:10 +0200

    added openssh

--- configuration.nix
+++ configuration.nix
@@ -24,6 +24,7 @@
     # Services
     ./modules/services/disk-management.nix
     ./modules/services/locate.nix
+    ./modules/services/openssh.nix
     ./modules/services/pipewire.nix
     ./modules/services/power-management.nix
     ./modules/services/printing.nix
