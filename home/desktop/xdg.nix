972f14526abc325bd6cdafe83d7a67aa2048e375
venefex <git@alexdidom.com>
2026-08-08 01:22:18 +0200

    added xdg screenshot dir

--- home/desktop/xdg.nix
+++ home/desktop/xdg.nix
@@ -1,4 +1,9 @@
-{ lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   nix.assumeXdg = true;
@@ -11,13 +16,14 @@
       enable = true;
       createDirectories = true;
       desktop = null;
+      screenshots = "${config.home.homeDirectory}/Pictures/Screenshots";
     };
   };
 
   # Use KDE's XDG application menu so KDE applications integrate correctly
   # outside of Plasma (e.g. Hyprland).
   xdg.configFile."menus/applications.menu".source =
-  "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+    "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
 
   # Create 'Programming' directory if not present
   home.activation.createProgrammingDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
