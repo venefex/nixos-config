63db9e47a09c50f620292509f7f60745103aea01
venefex <git@alexdidom.com>
2026-08-08 01:27:41 +0200

    added dunst

--- home/core/variables.nix
+++ home/core/variables.nix
@@ -4,6 +4,7 @@
   home.sessionVariables = {
     # Wayland
     NIXOS_OZONE_WL = "1";
+    GTK_USE_PORTAL = "1";
     screenshots = "${config.home.homeDirectory}/Pictures/Screenshots";
   };
 }
---
+++ home/programs/dunst.nix
@@ -0,0 +1,7 @@
+{ ... }:
+
+{
+  programs.dunst = {
+    enable = true;
+  };
+}
