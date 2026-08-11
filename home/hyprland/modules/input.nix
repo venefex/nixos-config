cfec14b4aa42997eda9a19f6e5f910bc8ce9c19f
venefex <git@alexdidom.com>
2026-08-11 23:24:43 +0200

    change keyboard layout

--- home/hyprland/modules/input.nix
+++ home/hyprland/modules/input.nix
@@ -3,9 +3,9 @@
 {
   wayland.windowManager.hyprland.settings.config.input = {
     kb_layout = "us";
-    kb_variant = "intl";
+    kb_variant = "altgr-intl";
     kb_model = "";
-    kb_options = "fkeys:basic_13-24";
+    kb_options = "fkeys:basic_13-24,lv3:ralt_switch"; # F13-F14,AltGr
     kb_rules = "";
     numlock_by_default = false;
 
