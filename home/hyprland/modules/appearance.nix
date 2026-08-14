5560b6c2d5a3ba578e728fe3dbea2d2bf6115a01
venefex <git@alexdidom.com>
2026-08-14 02:58:05 +0200

    qt improvements

--- home/hyprland/modules/appearance.nix
+++ home/hyprland/modules/appearance.nix
@@ -22,8 +22,8 @@ let
   ### White ###
   activeBorder = {
     colors = [
-      "#7aa2f7ff"
-      "#bb9af7ff"
+      "#cba6f7ee"
+      "#f5c2e7ee"
     ];
     angle = 45;
   };
@@ -34,7 +34,7 @@ in
     general = {
       gaps_in = 5;
       gaps_out = 10;
-      border_size = 2;
+      border_size = 3;
       col = {
         active_border = activeBorder;
         inactive_border = inactiveBorder;
@@ -52,13 +52,13 @@ in
       inactive_opacity = 1.0;
       dim_special = 0.4;
       shadow = {
-        enabled = true;
+        enabled = false;
         range = 4;
         render_power = 3;
         color = "0xee1a1a1a"; # or 4008636146 if a numeric form is required
       };
       blur = {
-        enabled = true;
+        enabled = false;
         size = 3;
         passes = 1;
         vibrancy = 0.1696;
