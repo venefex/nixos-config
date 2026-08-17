b84a5f73eff74d144cc4c72c11a43caeb5691d9d
venefex <git@alexdidom.com>
2026-08-17 02:33:12 +0200

    fixed noctalia session toggle exec_cmd

--- home/hyprland/modules/keybinds/noctalia.nix
+++ home/hyprland/modules/keybinds/noctalia.nix
@@ -22,7 +22,7 @@
     {
       _args = [
         (lib.generators.mkLuaInline ''mod .. " + End"'')
-        (lib.generators.mkLuaInline "hl.dsp.exec_cmd(noctalia msg panel-toggle session")
+        (lib.generators.mkLuaInline "hl.dsp.exec_cmd(noctalia msg panel-toggle session)")
       ];
     }
   ];
