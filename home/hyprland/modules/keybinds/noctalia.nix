43efc1fb0c130126d7a2c6a65590af45a97c4af4
venefex <git@alexdidom.com>
2026-08-17 04:03:49 +0200

    added noctalia control-center bind

--- home/hyprland/modules/keybinds/noctalia.nix
+++ home/hyprland/modules/keybinds/noctalia.nix
@@ -5,6 +5,15 @@
     # Launcher
     {
       _args = [
+        (lib.generators.mkLuaInline ''"SUPER + SUPER_L"'')
+        (lib.generators.mkLuaInline ''hl.dsp.exec_cmd("noctalia msg panel-toggle control-center")'')
+        (lib.generators.mkLuaInline ''{ release = true }'')
+      ];
+    }
+
+    # Launcher
+    {
+      _args = [
         (lib.generators.mkLuaInline ''mod .. " + Space"'')
         (lib.generators.mkLuaInline ''hl.dsp.exec_cmd("noctalia msg panel-toggle launcher")'')
       ];
